434af9faadb8976ab059c964cbb2e4e886492424
EmilioJeldes <emilio.jeldes@gmail.com>
2019-07-13 15:25:13 -0400

    first null-references commit

---
+++ NullReferences/src/cl/ejeldes/java/nullferences/NullReferences.java
@@ -0,0 +1,7 @@
+package cl.ejeldes.java.nullferences;
+
+/**
+ * Created by ejeldes on Jul 13, 2019
+ */
+public class NullReferences {
+}
---
+++ NullReferences/src/cl/ejeldes/kotlin/nullreferences/NullReferences.kt
@@ -0,0 +1,5 @@
+package cl.ejeldes.kotlin.nullreferences
+
+/**
+ * Created by ejeldes on Jul 13, 2019
+ */
